cc9944fe5c902775c5e372cccecdf1a4f0920020
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-17 17:08:55 -0400

    Created a db request for the budget database, created a onupgradedneeded function where we created the index and object store, created a request.onsuccess function along with an request.error function to notify a success or a error, created a saveRecord function where I created a transaction with readwrite access, a way in which to access object store, add a record to the store with an add method, created a checkDatabase to access all records from store and set variable, then created a getAll.onsuccess function for the data that is offlineand stringify the result and a then promise to return the json as well as another then promise in which I pseudo coded the next steps of what I need to do.

---
+++ public/indexedDb.js
@@ -0,0 +1,103 @@
+// function checkForIndexedDb() {
+//     window.indexedDB =
+//       window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+  
+//     window.IDBTransaction =
+//       window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+//     window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+  
+//     if (!window.indexedDB) {
+//       console.log("Your browser doesn't support a stable version of IndexedDB.");
+//       return false;
+//     }
+//     return true;
+//   }
+
+//   const request = window.indexedDB.open("toDoList", 1);
+
+//       // Create an object store inside the onupgradeneeded method.
+//       //onupgradeneeded is a universal method also. It is how we create a table.
+//       request.onupgradeneeded = ({ target }) => {
+//         const db = target.result;
+//         //const objectStore = db.createObjectStore("toDoList");
+
+//         const transaction = db.transaction(["toDoList"], "readwrite");
+//          //transaction means use todoList table and which objectstore are we dealing with? todoList
+//         const toDoListStore = transaction.objectStore("toDoList");//this is used to add data within line 35 down.
+//         const statusIndex = toDoListStore.index("statusIndex");
+//       };
+
+//       // On success console the result.
+//       request.onsuccess = event => {
+//         console.log(request.result);
+//       };
+
+let db;
+let count = 0;
+// create a new db request for a "budget" database.
+const request = indexedDB.open('budget', 1)
+
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  db = request.result
+  const objectStore = db.createObjectStore('pending', {keyPath: 'pendingID', autoIncrement: true})
+  objectStore.createIndex('pendingIndex', 'pendingValue')
+
+  console.log("")
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+};
+
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  // access your pending object store
+  // add record to your store with add method.
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const pendingStore = transaction.objectStore('pending')
+
+  pendingStore.add({pendingID: count.toString(), pendingValue: record.value})
+  count++
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  // access your pending object store
+  // get all records from store and set to a variable
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const budgetStore = transaction.objectStore('pending')
+  const getAll = budgetStore.openCursor()
+  
+//below code is  previously given:
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          // access your pending object store
+          // clear all items in your store
+          
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener('online', checkDatabase);
