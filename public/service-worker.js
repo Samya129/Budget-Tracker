876d8b2f105122e73d461e37d6c05a611eb43b73
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-17 16:43:30 -0400

    Made slight change to service-worker file and note.]

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,12 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/style.css",
-  "/seed.js",
+  "/manifest.webmanifest",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/manifest.webmanifest",
-];
+  "/indexedDb.js",
+]; // "/service-worker.js", ????
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
