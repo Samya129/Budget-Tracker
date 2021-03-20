210a9526603ea9092e39ab22381209843a19136a
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-19 23:09:13 -0400

    Added necessary code and notes for heroku to read port properly.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; //Necessdary for Heroku to listen to the port
 
 const app = express();
 
@@ -25,6 +25,7 @@ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budgetDb", {
 // routes
 app.use(require("./routes/api.js"));
 
+//Run server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
